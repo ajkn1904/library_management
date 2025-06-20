e4b5fe18648bdce1e559b1a511af50da521f260b
ajkn1904 <ajknishat@gmail.com>
2025-06-21 04:58:55 +0600

    get single, update and delete api added

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -4,7 +4,9 @@ import { Book } from "../models/books.model";
 export const booksRoutes = express.Router();
 
 //Create Book
-booksRoutes.post("/", async (req: Request, res: Response, next: NextFunction) => {
+booksRoutes.post(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       const body = req.body;
       const data = await Book.create(body);
@@ -17,20 +19,80 @@ booksRoutes.post("/", async (req: Request, res: Response, next: NextFunction) =>
     } catch (error) {
       next(error);
     }
-  });
+  }
+);
 
 //Get All Books
 booksRoutes.get(
   "/",
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-    
       const data = await Book.find();
 
       res.status(201).json({
         success: true,
         message: "Books retrieved successfully",
         data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// Get Book by ID
+booksRoutes.get(
+  "/:bookId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = req.params.bookId;
+      const data = await Book.findById(id);
+
+      res.status(201).json({
+        success: true,
+        message: "Books retrieved successfully",
+        data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+
+// Update Book
+booksRoutes.put(
+  "/:bookId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = req.params.bookId;
+      const newBody = req.body;
+      const data = await Book.findByIdAndUpdate(id, newBody, { new: true });
+
+      res.status(201).json({
+        success: true,
+        message: "Book updated successfully",
+        data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+
+// Delete Book
+booksRoutes.delete(
+  "/:bookId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = req.params.bookId;
+      const data = await Book.deleteOne({_id: id});
+
+      res.status(201).json({
+        success: true,
+        message: "Book deleted successfully",
+        data: null,
       });
     } catch (error) {
       next(error);
