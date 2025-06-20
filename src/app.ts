5d384aab83c1e1660c2fda6554412e91daa824df
ajkn1904 <ajknishat@gmail.com>
2025-06-21 04:17:51 +0600

    create and get api added

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,35 @@
-import express, { Application, Request, Response } from 'express';
-import { booksRoutes } from './app/controllers/books.controller';
-import { borrowRoutes } from './app/controllers/borrow.controller';
-
+import express, { Application, NextFunction, Request, Response } from "express";
+import { booksRoutes } from "./app/controllers/books.controller";
+import { borrowRoutes } from "./app/controllers/borrow.controller";
+import mongoose from "mongoose";
 
 const app: Application = express();
 
-app.use(express.json())
+app.use(express.json());
 
+app.use("/api/books", booksRoutes);
+app.use("/api/borrow", borrowRoutes);
 
-app.use("/api/books", booksRoutes)
-app.use("/api/borrow", borrowRoutes)
-
-
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Library Management App!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Library Management App!");
 });
 
+
+
+//Global error handler --> Generic Error Response
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  let statusCode = 500;
+  let errorResponse = {
+    success: false,
+    message: error.message,
+    error,
+  };
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    statusCode = 400;
+    errorResponse.message = "Validation failed";
+  }
+  res.status(statusCode).json(errorResponse);
+});
 
 export default app;
--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -1,6 +1,39 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from "express";
+import { Book } from "../models/books.model";
 
+export const booksRoutes = express.Router();
 
-export const booksRoutes = express.Router()
+//Create Book
+booksRoutes.post("/", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const body = req.body;
+      const data = await Book.create(body);
 
+      res.status(201).json({
+        success: true,
+        message: "Book created successfully",
+        data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 
+//Get All Books
+booksRoutes.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+    
+      const data = await Book.find();
+
+      res.status(201).json({
+        success: true,
+        message: "Books retrieved successfully",
+        data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
